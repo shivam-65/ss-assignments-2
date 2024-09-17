f8c17705c8d8b98a7d9e099331bbddeb60d63a34
shivam-65 <Padaliya.Prafulbhai@iiitb.ac.in>
2024-09-17 21:09:45 +0530

    ITIMER VIRTUAL

---
+++ 1b.c
@@ -0,0 +1,55 @@
+/*
+Name:- Padaliya Shivam
+Roll No:- MT2024107
+Program:- 1. Write a separate program (for each time domain) to set a interval timer in 10sec and
+10micro second
+a. ITIMER_REAL
+b. ITIMER_VIRTUAL
+c. ITIMER_PROF
+*/
+
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void main(int argc, char const *argv[])
+{
+    struct itimerval timer;
+    printf("Select interval timer\n1:10s\n2:10micros");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        timer.it_value.tv_sec = 10;
+        timer.it_value.tv_usec = 0;
+        break;
+    case 2:
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        timer.it_value.tv_sec = 0;
+        timer.it_value.tv_usec = 10;
+        break;
+    default:
+        perror("INVALID CHOICE");
+        _exit(1);
+        break;
+    }
+    int timerStatus = setitimer(ITIMER_VIRTUAL, &timer, 0);
+    if (timerStatus == -1)
+        perror("Error while setting an interval timer!");
+
+    while (1)
+        ;
+}
+
+/*
+shivam@shivam-Vostro-3590:~/Desktop/Handsonlist2/ss-assignments-2$ ./a.out
+Select interval timer
+1:10s
+2:10micros2
+Virtual timer expired
+*/
